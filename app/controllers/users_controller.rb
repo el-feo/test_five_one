2e864b26b6e035ccdcf69e8a82ba5ee1c799e715
el-feo <jebcoleman@gmail.com>
2017-05-02 17:04:42 -0700

    Adds confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,23 @@ class UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
-      render json: {status: 'User created successfully'}, status: :created
+      # TODO: Invoke send email method here
+      render json: { status: 'User created successfully' }, status: :created
     else
-      render json: {errors: user.errors.full_messages}, status: :bad_request
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def confirm
+    token = params[:token].to_s
+
+    user = User.find_by(confirmation_token: token)
+
+    if user.present? && user.confirmation_token_valid?
+      user.mark_as_confirmed!
+      render json: { status: 'User confirmed successfully' }, status: :ok
+    else
+      render json: { status: 'Invalid Token' }, status: :not_found
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: :create
+  resources :users, only: :create do
+    collection do
+      post 'confirm'
+    end
+  end
 end
